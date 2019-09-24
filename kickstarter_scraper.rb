898cc3773cca202a3872e30a32358280d555a98a
Levi Borland <borland.levi@gmail.com>
2019-09-24 21:59:16 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,8 @@
-# require libraries/modules here
-require nokogiri
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  
+  kickstarter = nokogiri::HTML(html)
+  
 end
